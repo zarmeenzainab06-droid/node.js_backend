c84aab07924e4812f36ff9cbd9f8236e24e7d195
unknown <zarmeenzainab06@gmail.com>
2026-06-24 12:35:34 +0500

    fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { login, signup } = require("../controllers/authController");
+const { login, signup } = require("../controllers/authcontroller");
 
 router.post("/login", login);
 router.post("/signup", signup);
