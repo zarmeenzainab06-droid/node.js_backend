e02ddc38d0adef001564f3aa35f29d54bd7171b3
Marsad <marsadakbar@gmail.com>
2026-06-28 12:33:03 +0500

    fix the code

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2/promise"); // Import mysql2 library with promise s
 
 const db = mysql.createPool({
   host: "localhost", // Database server is running on the local machine
-  user: "root", // MySQL username (default user in local setup)
-  password: "", // Password for MySQL user (empty in local development)
-  database: "healthy_wealthy_db", // Name of the database to connect to
+  user: "admin", // MySQL username (default user in local setup)
+  password: "YourStrongPassword", // Password for MySQL user (empty in local development)
+  database: "gym_app", // Name of the database to connect to
 }); // Create a pool of connections to efficiently manage multiple database requests
 
 module.exports = db; // Export the database pool so it can be used in other files of the project
